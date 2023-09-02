9067e67365a4bd903a13bc921422a24c7de54c60
Dunivixs <p.nom@ubuntu-fr.org>
2023-09-02 12:38:02 +0200

    forget version

--- rust/lib/src/lib.rs
+++ rust/lib/src/lib.rs
@@ -5,7 +5,7 @@ pub mod log_level;
 pub mod shortcut_trait;
 
 pub const DEFAULT_REMOTE_HOST: &str = "https://leria-etud.univ-angers.fr/~ddasilva/money_for_mima/";
-pub const VERSION: &str = "v1.0.0";
+pub const VERSION: &str = "v1.0.1";
 pub const CONFIG_FILE_NAME: &str = "config.json";
 
 pub enum ReturnValue {
